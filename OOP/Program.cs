03ebfb3ca0300d237dd36723e6d5ac942a459125
isakpro <isakram99@outlook.com>
2026-02-14 19:26:31 +0100

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -12,12 +12,11 @@ namespace OOP
         {
             Console.WriteLine("=== Bibliotekssystem med Arv ===\n");
 
-            // Skapa olika typer av biblioteksobjekt
+
             Book book1 = new Book("978-0-123456-78-9", "C# Programming", "Anders Andersson", 2020);
             Magazine magazine1 = new Magazine("MAG-001", "Computer Sweden", "IDG", 42, 2024);
             DVD dvd1 = new DVD("DVD-001", "The Matrix", "Lana Wachowski", 136, 1999);
 
-            // Lista med alla biblioteksobjekt (polymorfism)
             List<LibraryItem> libraryItems = new List<LibraryItem> { book1, magazine1, dvd1 };
 
             Console.WriteLine("=== Alla objekt i biblioteket ===\n");
@@ -27,13 +26,12 @@ namespace OOP
                 Console.WriteLine();
             }
 
-            // Skapa medlem
             Member member1 = new Member("M001", "Erik Johansson", "erik@example.com");
             
             Console.WriteLine("\n=== Medlem ===");
             Console.WriteLine(member1.GetMemberInfo());
 
-            // Låna olika typer av objekt
+
             Console.WriteLine("\n\n=== Lånar en bok ===");
             book1.IsAvailable = false;
             member1.AddBorrowedItem(book1);
@@ -52,11 +50,10 @@ namespace OOP
             Loan loan3 = new Loan(dvd1, member1, 3);
             Console.WriteLine(loan3.GetLoanInfo());
 
-            // Visa uppdaterad medlemsinformation
             Console.WriteLine("\n\n=== Uppdaterad medlemsinformation ===");
             Console.WriteLine(member1.GetMemberInfo());
 
-            // Returnera ett objekt
+
             Console.WriteLine("\n\n=== Returnerar boken ===");
             loan1.ReturnItem();
             Console.WriteLine(loan1.GetLoanInfo());
